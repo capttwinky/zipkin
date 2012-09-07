ad1e8b0d3ef7df7d7122db2e57afb2d0d4822af4
Joel McGrady <jmcgrady@twitter.com>
2012-09-07 15:42:42 -0700

    iterating on tpl

--- zipkin-hadoop/src/main/scala/com/twitter/zipkin/hadoop/TweetyPieLink.scala
+++ zipkin-hadoop/src/main/scala/com/twitter/zipkin/hadoop/TweetyPieLink.scala
@@ -26,44 +26,48 @@ import com.twitter.zipkin.hadoop.sources.{PrepTsvSource, PreprocessedSpanSource,
 */
 
 class TweetyPieLink(args: Args) extends Job(args) with DefaultDateRangeJob {
-
-  //Grab span info for this chunk
-  val spanInfo = PreprocessedSpanSource()
-  .read
-  	//.limit(10000)
-    .filter(0) { s : SpanServiceName => s.isSetParent_id() }
-    .mapTo(0 -> ('trace_id, 'parent_id, 'service))
-      { s: SpanServiceName => (s.trace_id, s.parent_id, s.service_name ) }
-
-  //Find all spans that called TP
-  val spanInfoTP = spanInfo
-      .filter('service){s: String => s.toLowerCase == "tweetypie"}  
-
-    // TODO: account for possible differences between sent and received service names
+  // TODO: account for possible differences between sent and received service names
   val idName = PrepTsvSource()
     .read
   
+  //Grab span info for this chunk
+  val spanInfo = PreprocessedSpanSource()
+  .read
+    .filter(0) { s : SpanServiceName => s.isSetParent_id() }
+    .filter(0) { s : SpanServiceName => s.isSetTrace_id() }
+    .mapTo(0 -> ('trace_id, 'parent_id, 'service))
+      { s: SpanServiceName => (s.trace_id, s.parent_id, s.service_name ) }
+    .filter('parent_id) {s: String => Option(s)  //Perform null check before filtering.
+      .map { _.toLowerCase != "" }
+      .getOrElse(false)
+    }
+    .filter('trace_id) {s: String => Option(s)  //Perform null check before filtering.
+      .map { _.toLowerCase != "" }
+      .getOrElse(false)
+    }
+    .joinWithSmaller('parent_id -> 'id_1, idName, joiner = new LeftJoin)
+    .rename(('name_1) -> ('parent_service))
+    .discard('id_1)
+    
   /* Join with the original on parent ID to get the parent's service name */
-  val spanInfoWithChildIsTweetyPie = spanInfoTP
-    .joinWithSmaller('parent_id -> 'id_1, idName, joiner = new LeftJoin)   //[{5}:'trace_id', 'parent_id', 'service', 'id_1', 'name_1']
-
+  //Find all spans that called TP
+  val spanInfoWithChildIsTweetyPie = spanInfo
+    .filter('service){s: String => s.toLowerCase == "tweetypie"}
+    
   //Go back to the original span list, and find backends that TweetyPie calls
   val spanInfoWithParentIsTweetyPie = spanInfo
-    .joinWithSmaller('parent_id -> 'id_1, idName, joiner = new LeftJoin)
-    .filter('name_1) {s: String => Option(s)  //Perform null check before filtering.
+    //.joinWithSmaller('parent_id -> 'id_1, idName, joiner = new LeftJoin)
+    .filter('parent_service) {s: String => Option(s)  //Perform null check before filtering.
       .map { _.toLowerCase == "tweetypie" }
       .getOrElse(false)
-    }   // Filter first?  Consider creating another val and moving above the join.
-    .rename(('trace_id, 'parent_id, 'service) -> ('trace_id_2, 'parent_id_2, 'service_2))
-    .project('trace_id_2, 'parent_id_2, 'service_2)
+    }
+    .rename(('trace_id, 'service) -> ('trace_id_2, 'called_service))
+    .discard('parent_id, 'parent_service)
 
   //Join the 'called by' and 'called' services by span ID
   val tweetypieJoin = spanInfoWithChildIsTweetyPie
       .joinWithSmaller('trace_id -> 'trace_id_2, spanInfoWithParentIsTweetyPie, joiner = new OuterJoin)
-      .project('trace_id, 'name_1, 'service_2)
-      .groupBy('trace_id, 'name_1, 'service_2){_.size}
-
-  /*  .project('trace_id, 'name_1, 'name_2, 'unicorn)
-      .groupBy('trace_id){ _.sortBy('trace_id) }    
- */   .write(Tsv(args("output")))
+      .project('trace_id, 'parent_service, 'called_service)
+      .groupBy('trace_id, 'parent_service, 'called_service){_.size}
+      .write(Tsv(args("output")))
 }
